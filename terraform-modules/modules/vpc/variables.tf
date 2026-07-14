231e45e05b3ac08f8cdf33e9065b574d727f01fc
shobhit . <shobhit.n@ibm.com>
2026-07-14 19:56:38 +0530

    Initial commit

---
+++ terraform-modules/modules/vpc/variables.tf
@@ -0,0 +1,21 @@
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+    type = list(string)
+}
+
+variable "private_subnets" {
+
+  type = list(string)
+
+}
+
+variable "availability_zones" {
+
+  type = list(string)
+
+}
+
+variable "cluster_name" {}
